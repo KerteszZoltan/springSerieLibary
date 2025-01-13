c3f2f8513f6aa43b383dc59e5f419d9bc5c0f236
ZoltanKertesz <kertesz.zoltan.9718@gmail.com>
2025-01-13 22:35:13 +0100

    listing all series

--- src/main/java/com/kerteszzoltan/springSeriLibrary/controllers/SerieController.java
+++ src/main/java/com/kerteszzoltan/springSeriLibrary/controllers/SerieController.java
@@ -1,9 +1,13 @@
 package com.kerteszzoltan.springSeriLibrary.controllers;
 
+import com.kerteszzoltan.springSeriLibrary.models.Serie;
 import com.kerteszzoltan.springSeriLibrary.services.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/serie")
@@ -14,5 +18,10 @@ public class SerieController {
     @Autowired
     public SerieController(SerieService serieService){
         this.serieService = serieService;
+    }
+
+    @GetMapping
+    public List<Serie> getSeries(){
+        return serieService.getSeries();
     }
 }
--- src/main/java/com/kerteszzoltan/springSeriLibrary/services/SerieService.java
+++ src/main/java/com/kerteszzoltan/springSeriLibrary/services/SerieService.java
@@ -1,8 +1,11 @@
 package com.kerteszzoltan.springSeriLibrary.services;
 
+import com.kerteszzoltan.springSeriLibrary.models.Serie;
 import com.kerteszzoltan.springSeriLibrary.repositories.ISerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SerieService {
@@ -11,5 +14,9 @@ public class SerieService {
     @Autowired
     public SerieService(ISerieRepository serieRepository){
         this.serieRepository = serieRepository;
+    }
+
+    public List<Serie> getSeries(){
+        return serieRepository.findAll();
     }
 }
